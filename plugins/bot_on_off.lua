dc46d2c84135e53cd0e8787727342bb8b0837f3d
badboyam <pouya001111@gmail.com>
2016-02-06 09:14:23 -0800

    Update bot_on_off.lua

--- plugins/bot_on_off.lua
+++ plugins/bot_on_off.lua
@@ -73,8 +73,8 @@ return {
 		"/bot on : enable robot in group",
 		"/bot off : disable robot in group" },
 	patterns = {
-		"^[!/]bot? (on)",
-		"^[!/]bot? (off)" }, 
+		"^bot? (on)",
+		"^bot? (off)" }, 
 	run = run,
 	privileged = true,
 	--moderated = true,
