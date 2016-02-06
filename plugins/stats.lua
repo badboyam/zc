ceecc804d30e7a7361651d5c05f564a268ec5c91
badboyam <pouya001111@gmail.com>
2016-02-06 09:36:55 -0800

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -138,11 +138,11 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[!/]([Ss]tats)$",
-    "^[!/]([Ss]tatslist)$",
-    "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (creedbot)",-- Put everything you like :)
-		"^[!/]([Cc]reedbot)"-- Put everything you like :)
+    "^([Ss]tats)$",
+    "^([Ss]tatslist)$",
+    "^([Ss]tats) (group) (%d+)",
+    "^([Ss]tats) (mirror)",-- Put everything you like :)
+		"^([Mm]irror)"-- Put everything you like :)
     }, 
   run = run
 }
